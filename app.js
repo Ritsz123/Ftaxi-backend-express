82c23f93b2f998a4fbec553325019878dd521812
ritesh <riteshkhadse12@gmail.com>
2021-06-07 11:25:19 +0530

    add. structured response

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 const authRoutes = require('./auth/routes/authRoutes');
+const { success, error } = require('./response');
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => { return res.send('Hello') })
+app.get('/', (req, res) => {
+    const responseBody = [
+        {
+            data: 'Hello world'
+        },
+        {
+            data: 'No world'
+        },
+    ]
+
+    res.status(200).json(success('ok', responseBody, res.statusCode));
+})
+
+app.get('/error', (req, res) => {
+    const errorBody = [{ authentication: 'Unauthorised request' }]
+
+    res.status(400).json(error('Failed', errorBody, res.statusCode));
+})
 
 
 app.listen(5001, () => console.log('Started server on http://localhost:5001'));
